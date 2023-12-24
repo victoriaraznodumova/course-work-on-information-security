ca0a2a9f3ab2bccc768080394a8ce07553b71af3
Виктория Разнодумова <raznodumova@gmail.com>
2023-12-25 00:36:06 +0300

    start working with methods and buttons

--- src/App.vue
+++ src/App.vue
@@ -25,12 +25,12 @@ import TheWelcome from './components/TheWelcome.vue'
 
 
     <p style="white-space: pre-line;"></p>
-    <textarea id="key-textarea" rows="7" cols="38" v-model="key" placeholder="Введите ключ"></textarea>
+    <textarea id="key-textarea" rows="7" cols="38" type="text" v-model="key" placeholder="Введите ключ"></textarea>
     <p style="white-space: pre-line;"></p>
     <textarea id="encrypt-textarea" rows="7" cols="38" v-model="message" placeholder="Введите текст для шифрования"></textarea>
     <br>
     <div class = "buttons">
-      <button v-on:click="displayEncryption(Encryption2())" type="button" class="btn btn-dark">Зашифровать</button>
+      <button v-on:click="displayEncryption(Encryption())" type="button" class="btn btn-dark">Зашифровать</button>
       <button v-on:click="displayDecryption(keyExpantion3(key))" type="button" class="btn btn-dark ">Расшифровать</button>
     </div>
     <p style="white-space: pre-line;"></p>
@@ -43,47 +43,11 @@ import TheWelcome from './components/TheWelcome.vue'
 export default {
     data() {
         return {
-            exampleGraph: [
-                [0, 6, 2, 10, 0],
-                [0, 0, 1, 0, 6],
-                [0, 0, 0, 9, 3],
-                [0, 0, 0, 0, 7],
-                [0, 0, 0, 0, 0],
-            ],
-            source: 0,
-            sink: 0,
-            maxFlow: -1,
-            nodesNumberInput: 2,
-            nodesNumber: 0,
-            matrixInput: [[0, 0], [0, 0]],
-            matrix: null,
-            paths: [],
-            pathsOutput: [],
-            isReadyForGraph: false,
-
-
-
-
+            
             input: "",
-            //key: "",
+            key: "",
             output: "",
             roundKeyCount: 64,
-            sBox: [0xd9, 0x78, 0xf9, 0xc4, 0x19, 0xdd, 0xb5, 0xed, 0x28, 0xe9, 0xfd, 0x79, 0x4a, 0xa0, 0xd8, 
-            0x9d, 0xc6, 0x7e, 0x37, 0x83, 0x2b, 0x76, 0x53, 0x8e, 0x62, 0x4e, 0x64, 0x88, 0x44, 0x8b, 0xfb, 0xa2, 
-            0x17, 0x9a, 0x59, 0xf5, 0x87, 0xb3, 0x4f, 0x13, 0x61, 0x45, 0x6d, 0x8d, 0x09, 0x81, 0x7d, 0x32, 
-            0xbd, 0x8f, 0x40, 0xeb, 0x86, 0xb7, 0x7b, 0x0b, 0xf0, 0x95, 0x21, 0x22, 0x5c, 0x6b, 0x4e, 0x82, 
-            0x54, 0xd6, 0x65, 0x93, 0xce, 0x60, 0xb2, 0x1c, 0x73, 0x56, 0xc0, 0x14, 0xa7, 0x8c, 0xf1, 0xdc, 
-            0x12, 0x75, 0xca, 0x1f, 0x3b, 0xbe, 0xe4, 0xd1, 0x42, 0x3d, 0xd4, 0x30, 0xa3, 0x3c, 0xb6, 0x26, 
-            0x6f, 0xbf, 0x0e, 0xda, 0x46, 0x69, 0x07, 0x57, 0x27, 0xf2, 0x1d, 0x9b, 0xbc, 0x94, 0x43, 0x03, 
-            0xf8, 0x11, 0xc7, 0xf6, 0x90, 0xef, 0x3e, 0xe7, 0x06, 0xc3, 0xd5, 0x2f, 0xc8, 0x66, 0x1e, 0xd7, 
-            0x08, 0xe8, 0xea, 0xde, 0x80, 0x52, 0xee, 0xf7, 0x84, 0xaa, 0x72, 0xac, 0x35, 0x4d, 0x6a, 0x2a, 
-            0x96, 0x1a, 0xd2, 0x71, 0x5a, 0x15, 0x49, 0x74, 0x4b, 0x9f, 0xd0, 0x5e, 0x04, 0x18, 0xa4, 0xec, 
-            0xc2, 0xe0, 0x41, 0x6e, 0x0f, 0x51, 0xcb, 0xcc, 0x24, 0x91, 0xaf, 0x50, 0xa1, 0xf4, 0x70, 0x39, 
-            0x99, 0x7c, 0x3a, 0x85, 0x23, 0xb8, 0xb4, 0x7a, 0xfc, 0x02, 0x36, 0x5b, 0x25, 0x55, 0x97, 0x31, 
-            0x2d, 0x5d, 0xfa, 0x98, 0xe3, 0x8a, 0x92, 0xae, 0x05, 0xdf, 0x29, 0x10, 0x67, 0x6c, 0xba, 0xc9, 
-            0xd3, 0x00, 0xe6, 0xcf, 0xe1, 0x9e, 0xa8, 0x2c, 0x63, 0x16, 0x01, 0x3f, 0x58, 0xe2, 0x89, 0xa9, 
-            0x0d, 0x38, 0x34, 0x1b, 0xab, 0x33, 0xff, 0xb0, 0xbb, 0x48, 0x0c, 0x5f, 0xb9, 0xb1, 0xcd, 0x2e, 
-            0xc5, 0xf3, 0xdb, 0x47, 0xe5, 0xa5, 0x9c, 0x77, 0x0a, 0xa6, 0x20, 0x68, 0xfe, 0x7f, 0xc1, 0xad],
             expandedKey: [],
 
         }
@@ -97,9 +61,6 @@ export default {
       },
 
       
-
-
-
       keyExpantion3 (key){
         return [15, 45, 64, 226, 36, 164, 155, 5];
       },
@@ -161,16 +122,16 @@ export default {
                   temp = applySBox(leftShift(temp, 8)) ^ (0x1B << 24);
                 }
 
-                const prevWord = expandedKey[i - 4];
+                let prevWord = expandedKey[i - 4];
                 expandedKey.push(prevWord ^ temp);
               } else {
-                const prevWord = expandedKey[i - 4];
-                const temp = expandedKey[i - 1];
+                let prevWord = expandedKey[i - 4];
+                let temp = expandedKey[i - 1];
                 expandedKey.push(prevWord ^ temp);
               }
             }
 
-            return expandedKey.toString(16);
+            return expandedKey;
           }
 
             // Пример использования
@@ -180,14 +141,16 @@ export default {
 
 
 
-            expandedKey = [21, 5, 25, 223, 54, 140, 230,  19];
-            return expandedKey.toString(); 
+            //expandedKey = [21, 5, 25, 223, 54, 140, 230,  19];
+            return expandedKey; 
 
             //return expandedKey.map(word => word);
             
         },
 
-
+        Key(){
+            return [43,126,21,22,452984875,452984917,452984896,452984918,43,452984958,62,452984936,452984875,85,107,452984835,43,126,21,452984854,452984875,452984917,452984896,86,43,452984958,62,104,452984875,85,107,3,43,126,21,22,452984875,452984917,452984896,452984918,43,452984958,62,452984936,452984875,85,107,452984835,43,126,21,452984854,452984875,452984917,452984896,86,43,452984958,62,104,452984875,85,107,3]
+          },
 
 
 
@@ -198,19 +161,15 @@ export default {
               const f = (key[i] ^ (R[(i-1) % 4] && R[(i-2) % 4]) ^ ((R[(i-1) % 4] ^ R[(i-1) % 4]) && R[(i-3) % 4])) & 0xFFFF;
               const value = (R[i] ^ f) & 0xFFFF;
               result.push(value)
-          
-          //result.push(value.toString(16))
-        
             }
             return result;
-      //return result.toString(16);
           }
 
           function mash_encryption (R, key){
             const result = [];
               for (let i = 0; i<4; i++){
                   let n = R[(i-1) % 4] && 63;
-                  value = (R[i] ^ key[n]) & 0xFFFF;
+                  let value = (R[i] ^ key[n]) & 0xFFFF;
                   result.push(value)
               }
               return result;
@@ -220,25 +179,20 @@ export default {
 
   //15, 45, 64, 226, 36, 164, 155, 5
 
-
-
-
   //0x2b, 0x7e, 0x15, 0x16, 0x77, 0xbd, 0x8b, 0x99
   //43, 126, 15, 16, 77, 189, 139, 99
 
           const R_14 = R.slice(0, 4);
           const R_58 = R.slice(4, 8);
 
-          key = [43,126,21,22,452984875,452984917,452984896,452984918,43,452984958,62,452984936,452984875,85,107,452984835,43,126,21,452984854,452984875,452984917,452984896,86,43,452984958,62,104,452984875,85,107,3,43,126,21,22,452984875,452984917,452984896,452984918,43,452984958,62,452984936,452984875,85,107,452984835,43,126,21,452984854,452984875,452984917,452984896,86,43,452984958,62,104,452984875,85,107,3];
-
+          let enc_key = this.keyExpantion();
           const sliced_key_encryption = [];
 
-          for (let i = 0; i < key.length; i += 4) {
-            sliced_key_encryption.push(key.slice(i, i + 4));
+          for (let i = 0; i < enc_key.length; i += 4) {
+            sliced_key_encryption.push(enc_key.slice(i, i + 4));
           }
 
-          //console.log("encryption");
-          //first 5 mix rounds
+
           const R_14_encryption_1 = mix_encryption(R_14, sliced_key_encryption[0]);
           //console.log ("mix 1st round", R_14_encryption_1);
           const R_58_encryption_1 = mix_encryption(R_58, sliced_key_encryption[1]);
@@ -251,12 +205,10 @@ export default {
           //console.log ("mix 5th round", R_14_encryption_3);
 
 
-          //first mash round
-          const R_58_encryption_3 = mash_encryption(R_58_encryption_2, key);
+          const R_58_encryption_3 = mash_encryption(R_58_encryption_2, enc_key);
           //console.log ("mash 1st round", R_58_encryption_3);
 
 
-          //nest 6 mix rounds
           const R_14_encryption_4 = mix_encryption(R_14_encryption_3, sliced_key_encryption[5]);
           //console.log ("mix 6th round", R_14_encryption_4);
           const R_58_encryption_4 = mix_encryption(R_58_encryption_3, sliced_key_encryption[6]);
@@ -271,12 +223,10 @@ export default {
           //console.log ("mix 11th round", R_58_encryption_6);
 
 
-          //second mash round
-          const R_14_encryption_7 = mash_encryption(R_14_encryption_6, key);
+          const R_14_encryption_7 = mash_encryption(R_14_encryption_6, enc_key);
           //console.log ("mash 2nd round", R_14_encryption_7);
 
 
-          //next 5 mix rounds
           const R_58_encryption_7 = mix_encryption(R_58_encryption_6, sliced_key_encryption[11]);
           //console.log ("mix 12th round", R_58_encryption_7);
           const R_14_encryption_8 = mix_encryption(R_14_encryption_7, sliced_key_encryption[12]);
@@ -288,22 +238,22 @@ export default {
           const R_58_encryption = mix_encryption(R_58_encryption_8, sliced_key_encryption[15]);
           //console.log ("mix 16th round", R_58_encryption);
 
-          //result of encryption
+          
+
           const encryption = R_14_encryption.concat(R_58_encryption);
-
-          //console.log("encryption: ", encryption);
-          //return encryption.toString();
-          return 5;
-
+          const hexArray = encryption.map(item => item.toString(16));
+          return hexArray;
 
           },
 
 
 
+          
 
 
 
 
+          
 
 
 
